7a26dc2615d9b512e9cf2f68e0dd38cf1a970686
Niels van der Molen <niels@wisvids.nl>
2021-10-25 13:02:39 +0200

    Fix export

--- svelte/functions/Custom/Trigger/exportData.js
+++ svelte/functions/Custom/Trigger/exportData.js
@@ -54,18 +54,140 @@ exports.manuallyTrigger = functions.firestore.document('/triggers/{triggerId}')
     return null;
   });
 
+async function getGoalConnectionExport(db, goalExport, connectionName) {
+  let connectionColRef = db.collection("goals").doc(goalExport.id).collection(connectionName);
+  return connectionColRef.get().then((connectionQuerySnap) => {
+    let i = 0;
+    connectionQuerySnap.forEach((connectionSnap) => {
+      let goalConnection = connectionSnap.data();
+      if (goalConnection.status === 'published') {
+        goalExport["goal_links_" + connectionName + "_" + i + "_id"] = goalConnection.connectionGoalId;
+        i = i + 1;
+      }
+    });
+    return goalExport;
+  }).catch((error) => {
+    console.log("Error getting documents: ", error);
+  });
+}
+
+async function getAdventureExport(db, goalExport, adventuresExport) {
+  let adventuresColRef = db.collection("goals").doc(goalExport.id).collection('adventures');
+  return adventuresColRef.get().then((adventuresQuerySnap) => {
+    let i = 0;
+    adventuresQuerySnap.forEach((adventureSnap) => {
+      let adventure = adventureSnap.data();
+      if (adventure.status === 'published') {
+        let adventureExport = {
+          id: adventureSnap.id,
+          title: adventure.title,
+          goalId: goalExport.id,
+        };
+        adventuresExport.push(adventureExport);
+      }
+    });
+    return adventuresExport;
+  }).catch((error) => {
+    console.log("Error getting documents: ", error);
+  });
+}
+
+// db.collection("goals").doc(goalSnap.id).collection('adventures');
+// adventuresColRef.get().then((adventuresQuerySnap) => {
+//   console.log('adventure query');
+//   adventuresQuerySnap.forEach((adventureSnap) => {
+//     console.log('adventure snap');
+//     let adventure = adventureSnap.data();
+//     if (adventure.status === 'published') {
+//       let adventureExport = {
+//         id: adventureSnap.id,
+//         title: adventure.title,
+//         goalId: goalExport.id,
+//       };
+//       adventureExports.push(adventureExport);
+//     }
+//   });
+//   return adventureExports;
+// }).catch((error) => {
+//   console.log("Error getting documents: ", error);
+// });
+
 async function dataExport() {
   const fb = helper.getFirebaseApp();
   let db = fb.firestore();
-  let goalsQuery = db.collection("goals");
-  let goalsQuerySnap = await goalsColRef.get();
-  let goals = [];
-  goalsQuerySnap.forEach((goalSnap) => {
-    let goal = goalSnap.data();
-    goal = {};
-    goal.title = goalData.title;
-    goal.id = goalDoc.id;
+
+  let goalsColRef = db.collection("goals");
+  let goalsExport = await goalsColRef.get().then((goalsQuerySnap) => {
+    let goals = [];
+    goalsQuerySnap.forEach((goalSnap) => {
+      let goal = goalSnap.data();
+      let goalExport = {};
+      goalExport.title = goal.title;
+      goalExport.id = goalSnap.id;
+      goals.push(goalExport);
+    }
+    );
+    return goals;
+  }).catch((error) => {
+    console.log("Error getting documents: ", error);
   });
+
+  let adventuresExport = [];
+  for (let i = 0; i < goalsExport.length; i++) {
+    goalsExport[i] = await getGoalConnectionExport(db, goalsExport[i], 'prerequisites');
+    goalsExport[i] = await getGoalConnectionExport(db, goalsExport[i], 'specializations');
+    goalsExport[i] = await getGoalConnectionExport(db, goalsExport[i], 'bigideas');
+    goalsExport[i] = await getGoalConnectionExport(db, goalsExport[i], 'deeperunderstandings');
+    adventuresExport = await getAdventureExport(db, goalsExport[i], adventuresExport);
+  }
+
+  let feedbackExport = await db.collection("feedback").get().then((querySnapshot) => {
+    let feedbackExportData = [];
+    querySnapshot.forEach((docSnap) => {
+      let feedbackData = docSnap.data();
+      if (feedbackData.adventureId) {
+        let exportData = {
+          adventureId: feedbackData.adventureId,
+          feedbackValue: feedbackData.feedbackValue,
+          time: feedbackData.time,
+          uid: feedbackData.uid
+        }
+        switch (feedbackData.feedbackValue) {
+          case -1:
+            exportData.feedback = 'too-difficult';
+            break;
+          case -0.5:
+            exportData.feedback = 'too-easy';
+            break;
+          case 1:
+            exportData.feedback = 'just-right';
+            break;
+        }
+
+        feedbackExportData.push(exportData);
+      }
+    });
+    return feedbackExportData;
+
+  }).catch(err => {
+    functions.logger.error(err.message);
+  });
+
+
+
+  let goalsExportJson = JSON.stringify(goalsExport);
+  let adventuresExportJson = JSON.stringify(adventuresExport);
+  let feedbackExportJson = JSON.stringify(feedbackExport);
+
+  let exportData = {
+    adventures: adventuresExportJson,
+    goals: goalsExportJson,
+    feedback: feedbackExportJson,
+    createdAt: Date.now()
+  }
+
+  return db.collection("dataExport").add(exportData);
+
 
 
   // let adventuresRef = db.collection("activities");

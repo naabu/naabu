8961539c7b0f63b86cc7c949dfa40d01c7d3fdf9
Niels van der Molen <niels@wisvids.nl>
2021-11-01 09:54:26 +0100

    Full name required in Curriculum profile

--- svelte/src/lib/Curriculum/form.svelte
+++ svelte/src/lib/Curriculum/form.svelte
@@ -21,6 +21,7 @@
             name="fullname"
             id="fullname"
             autocomplete="name"
+     
             bind:value={curriculumProfile.fullname}
             class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
           />

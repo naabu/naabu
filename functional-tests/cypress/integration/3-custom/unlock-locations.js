aca6f589a20d03ecaefaf2651fe38d39df5b5379
Niels van der Molen <nielsvandermolen@gmail.com>
2021-12-13 12:34:12 +0100

    Fix unlocking location test

--- functional-tests/cypress/integration/3-custom/unlock-locations.js
+++ functional-tests/cypress/integration/3-custom/unlock-locations.js
@@ -6,18 +6,28 @@ const getIframeOfVimeo = () => {
     .then(cy.wrap)
 }
 
+
+
+beforeEach(() => {
+  cy.clearLocalStorage();
+  cy.clearCookies();
+  cy.clearFirebaseAuth();
+});
+
 describe("Unlock location on map test", () => {
   it("Goes to the map. Visits a location and goes on adventure. Should unlock locations.", () => {
-    cy.visit('http://sveltekit:3000/cypress/login-normal-user').contains('Login complete');
-    cy.visit('http://sveltekit:3000/cypress/reset-map').contains('Player map is deleted!', {timeout: 100000});
-    cy.get(':nth-child(1) > [data-cy=logo]').click();
+    // cy.visit('http://sveltekit:3000/cypress/login-normal-user').contains('Login complete');
+    // cy.visit('http://sveltekit:3000/cypress/reset-map').contains('Player map is deleted!', {timeout: 100000});
+    // cy.get(':nth-child(1) > [data-cy=logo]').click();
+    cy.visit('http://sveltekit:3000');
+    cy.get("[data-cy=module-0-title-link]").click();
     cy.get('.locked_marker_1').first().should('be.visible')
     cy.get('#map').not('.unlocked_marker_1');
-    cy.get('.unlocked_marker_0').first().should('be.visible').click({ force: true });   
+    cy.get('.unlocked_marker_0').first().should('be.visible').click({ force: true });
     // Click adventure link.
-    cy.get('[href="/e0gBgbnAtztC78gHjhsb/IoangRp3XdQCGi86C4fz"] > .col-span-1').click();
+    cy.get('[href="/QWfmPv9xHqY0uHzOJ3RC/uu7vp/jVtQKV64D76sITvxNpVP"] > .col-span-1').click();
     getIframeOfVimeo().find('.play').click()
-    .get(':nth-child(1) > .text-lg').contains('Can you press the correct answer Cypress?').should('be.visible');
+      .get(':nth-child(1) > .text-lg').contains('Can you press the correct answer Cypress?').should('be.visible');
     getIframeOfVimeo().get('[data-cy=watch-image-video-button]').click();
     getIframeOfVimeo().get('[data-cy=back-to-quiz-button]').click();
     getIframeOfVimeo().get('[data-cy=input-value-1]').click();
@@ -28,26 +38,32 @@ describe("Unlock location on map test", () => {
     getIframeOfVimeo().not('[data-cy=incorrect-feedback]');
     getIframeOfVimeo().get('[data-cy=correct-feedback]').should('be.visible');
     getIframeOfVimeo().get('[data-cy=continue-button]').click();
-    getIframeOfVimeo().get('[data-cy=just-right]').should('be.visible').click();
-    cy.get('.unlocked_marker_1').first().should('be.visible')
-    cy.get('#map').not('.locked_marker_1');
-    cy.get('.unlocked_marker_1').first().click({ force: true });
-    cy.get('#map').not('.unlocked_marker_3');
-    // Click adventure link.
-    cy.get('[href="/YRuPvnC4jJCQisbrgSb6/4EZ3exPKUfyn8SyyejT7"] > .col-span-1').click();
-    cy.get('[data-cy=attack_v1]').should('be.visible'). should('be.disabled');
-    cy.get('[data-cy=answer_v2_1]').click();
-    cy.get('[data-cy=attack_v1]').click();
-    cy.get('[data-cy=defeated').should('be.visible');
+    getIframeOfVimeo().get('[data-cy=too-easy]').should('be.visible').click();
+    cy.get('[data-cy=activity-teaser-0]').contains("a12");
+    cy.get('[href="/QWfmPv9xHqY0uHzOJ3RC/uu7vp/v4OFhbHO772Fh1U5uczv"] > .col-span-1').click();
+    // cy.get('[data-cy=attack_v1]').should('be.visible').should('be.disabled');
+    // cy.get('[data-cy=answer_v2_1]').click();
+    // cy.get('[data-cy=attack_v1]').click();
+    // cy.get('[data-cy=defeated').should('be.visible');
     getIframeOfVimeo().find('.play').click()
     getIframeOfVimeo().get('[data-cy=too-easy]').should('be.visible').click();
-    cy.get('[data-cy=attack_v1]').should('be.visible'). should('be.disabled');
-    cy.get('[data-cy=answer_v2_2]').click();
-    cy.get('[data-cy=attack_v1]').click();
-    cy.get('[data-cy=won').should('be.visible');
-    cy.get('.unlocked_marker_3').first().should('be.visible');
-    cy.get('#map').not('.locked_marker_3');
-    cy.visit('http://sveltekit:3000/cypress/reset-map').contains('Player map is deleted!', {timeout: 100000});
+    cy.get('[data-cy=activity-teaser-0]').contains("a13");
+    cy.get('[href="/QWfmPv9xHqY0uHzOJ3RC/uu7vp/CWNBZVt5bypB4MbGaLqV"] > .col-span-1').click();
+    getIframeOfVimeo().find('.play').click()
+    getIframeOfVimeo().get('[data-cy=too-easy]').should('be.visible').click();
+
+    cy.get('[data-cy=new-location-unlocked]').should('be.visible');
+    cy.get('[data-cy=back-to-course-button]').click();
+    cy.get('#map').not('.locked_marker_1');
+    cy.get('.unlocked_marker_1').first().should('be.visible').click({ force: true });
+    cy.get('[data-cy=activity-teaser-0]').contains("a31");
+    // cy.get('[data-cy=attack_v1]').should('be.visible').should('be.disabled');
+    // cy.get('[data-cy=answer_v2_2]').click();
+    // cy.get('[data-cy=attack_v1]').click();
+    // cy.get('[data-cy=won').should('be.visible');
+    // cy.get('.unlocked_marker_3').first().should('be.visible');
+    // cy.get('#map').not('.locked_marker_3');
+    // cy.visit('http://sveltekit:3000/cypress/reset-map').contains('Player map is deleted!', { timeout: 100000 });
   });
 });
 

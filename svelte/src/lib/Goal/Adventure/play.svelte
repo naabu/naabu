abd74e7f8fe65c9c8866ddf17e4122cf0b2697cd
Niels van der Molen <niels@wisvids.nl>
2022-01-06 12:35:23 +0100

    Delete all things related to adventures

--- svelte/src/lib/Goal/Adventure/play.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import ShowActivity from "$lib/Activity/show.svelte";
-  export let firebase;
-  export let adventure;
-</script>
-
-<div class="mt-8 mb-32">
-  <ShowActivity bind:firebase bind:activity={adventure} />
-</div>

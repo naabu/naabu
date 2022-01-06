0edbe3c9b1084a6009086dd27614d6812b13bc3c
Niels van der Molen <niels@wisvids.nl>
2022-01-06 10:30:35 +0100

    Remove management pages

--- svelte/src/lib/Tabs/management.svelte
+++ svelte/src/lib/Tabs/management.svelte
@@ -4,30 +4,10 @@
 
   let mainTabs = [
     {
-      value: "overview",
-      text: "Overview",
-      url: "/beheer",
-    },
-    {
-      value: "activity",
-      text: "Activiteiten",
-      url: "/beheer/activiteit",
-    },
-    {
-      value: "goal",
-      text: "Leerdoelen",
-      url: "/beheer/leerdoel",
-    },
-    {
       value: "map",
       text: "Kaarten",
       url: "/beheer/kaart",
-    },
-    {
-      value: "user",
-      text: "Gebruikers",
-      url: "/beheer/gebruiker",
-    },
+    }
   ];
 </script>
 

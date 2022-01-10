342077a7067fd4bb1db48441facd0c5b160cd82f
Niels van der Molen <niels@wisvids.nl>
2022-01-10 13:07:48 +0100

    Add config scripts that work

--- scripts/generateClientConfig.js
+++ scripts/generateClientConfig.js
@@ -1,18 +1,15 @@
 
 import * as fs from 'fs';
-
 let environment = "development";
 
 if (process.env.environment) {
   environment = process.env.environment;
 }
 
-// config-algolia.js
-// config-firebase.js
 let algoliaConfigFile = fs.readFileSync('/var/www/config/config-algolia.js', 'utf8');
 let firebaseConfigFile = fs.readFileSync('/var/www/config/config-firebase.js', 'utf8');
 
-let projectIdRegex = new RegExp("projectId: \"(.*)\"", "g");
+let projectIdRegex = new RegExp("projectId:\\s*[\"'](.*)[\"']", "g");
 let match = projectIdRegex.exec(firebaseConfigFile);
 let projectId = match[1];
 let firebaseRCFileObject = {
@@ -29,26 +26,15 @@ algoliaConfigFile = "export " + algoliaConfigFile;
 fs.writeFile('/var/www/src/lib/Internals/Firebase/config.js', firebaseConfigFile, (err) => {
   // throws an error, you could also catch it here
   if (err) throw err;
-
-  // success case, the file was saved
-  console.log('Firebase config file saved!');
 });
 
 fs.writeFile('/var/www/src/lib/Internals/Algolia/config.js', algoliaConfigFile, (err) => {
   // throws an error, you could also catch it here
   if (err) throw err;
-
-  // success case, the file was saved
-  console.log('Algolia config file saved!');
 });
 
 fs.writeFile('/var/www/.firebaserc', jsonFirebaseRCFile, (err) => {
   // throws an error, you could also catch it here
   if (err) throw err;
-
-  // success case, the file was saved
-  console.log('Firebaserc project file saved!');
 });
-
-
 
--- scripts/generateServerConfig.js
+++ scripts/generateServerConfig.js
@@ -1,5 +1,11 @@
 
 import * as fs from 'fs';
+import("./generateClientConfig.js");
+
+let algoliaConfigFile = fs.readFileSync('/var/www/config/config-algolia.js', 'utf8');
+let applicationIDRegex = new RegExp("applicationID:\\s*[\"'](.*)[\"']", "g");
+let match = applicationIDRegex.exec(algoliaConfigFile);
+let applicationID = match[1];
 
 let environment = "development";
 
@@ -7,51 +13,17 @@ if (process.env.environment) {
   environment = process.env.environment;
 }
 
-// config-algolia.js
-// config-firebase.js
+let algoliaApiKey = fs.readFileSync('/run/secrets/algolia-private-api-key', 'utf8');
 
-let algoliaConfigFile = fs.readFileSync('/var/www/config/config-algolia.js', 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err);
-    return
+let objectToJson = {
+  algolia: {
+    app: applicationID,
+    key: algoliaApiKey
+  },
+  app: {
+    environment: environment
   }
-  console.log(data);
-  return data;
-});
-console.log(algoliaConfigFile);
-
-if (process.env.algoliaApp) {
-  let algoliaApp = process.env.algoliaApp;
-
-  let algoliaApiKey;
-  if (process.env.algoliaApiKey) {
-    algoliaApiKey = process.env.algoliaApiKey;
-  }
-  else {
-    algoliaApiKey = fs.readFileSync('/run/secrets/algolia-private-api-key', 'utf8' , (err, data) => {
-      if (err) {
-        console.error(err);
-        return
-      }
-      return data;
-    });
-  }
-
-  let objectToJson = {
-    algolia: {
-      app: algoliaApp,
-      key: algoliaApiKey
-    },
-    app: {
-      environment: environment
-    }
-  }
-
-  let json = JSON.stringify(objectToJson, null, 2);
-  fs.writeFileSync("/var/www/functions/.runtimeconfig.json", json);
-
-}
-else {
-  console.error("algoliaApp environment variable is not defined");
 }
 
+let json = JSON.stringify(objectToJson, null, 2);
+fs.writeFileSync("/var/www/functions/.runtimeconfig.json", json);

76cc56957032d362a3cf71b6238ff4a96645b52b
Niels van der Molen <niels@wisvids.nl>
2022-01-27 11:38:13 +0100

    Fix bug with connection lists data tables

---
+++ src/routes/leerdoel/[id]/[connectionType]/[connectionId]/index.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { getStores, page } from "$app/stores";
+  import ConnectionPage from "$lib/Goal/Connection/Components/page.svelte";
+  import ConnectionTemplate from "$lib/Internals/Containers/connectionTemplate.svelte";
+  import { getDefaultGoalBreadcrumbs } from "$lib/Goal/Components/helper";
+  import GetConnectionData from "$lib/Goal/Connection/Data/getConnectionData.svelte";
+  import GetGoalData from "$lib/Goal/Data/getGoalData.svelte";
+
+  let goal;
+  let connection;
+
+  let firebase;
+  let breadcrumbs;
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  $: if (goal) {
+    breadcrumbs = getDefaultGoalBreadcrumbs(goal);
+
+    breadcrumbs = [
+      ...breadcrumbs,
+      {
+        url: "/leerdoel/" + goal.id + "/" + $page.params.connectionType,
+        value: capitalizeFirstLetter($page.params.connectionType),
+      },
+      {
+        url: $page.path,
+        value: capitalizeFirstLetter($page.params.connectionType) + " connectie pagina",
+      },
+    ];
+  }
+</script>
+
+<GetGoalData bind:firebase bind:goal />
+<GetConnectionData bind:firebase bind:connection />
+
+<ConnectionTemplate bind:goal bind:firebase bind:breadcrumbs>
+  <ConnectionPage bind:goal bind:firebase bind:connection />
+</ConnectionTemplate>
---
+++ src/routes/leerdoel/[id]/[connectionType]/prullenbak.svelte
@@ -0,0 +1,14 @@
+<script>
+  import ConnectionListContainer from "$lib/Internals/Containers/connectionListContainer.svelte";
+  import { page } from "$app/stores";
+  let urlType = $page.params.connectionType;
+</script>
+
+<ConnectionListContainer
+  status="in-trash"
+  bind:urlType
+  let:goal
+  let:connections
+  let:firebase
+  let:status
+/>

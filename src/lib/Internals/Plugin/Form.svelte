491e218f7200037cde4a2bbeb733c7a29e6bd069
Niels van der Molen <niels@wisvids.nl>
2022-03-30 11:25:34 +0200

    Delete top plugin

--- src/lib/Internals/Plugin/Form.svelte
+++ src/lib/Internals/Plugin/Form.svelte
@@ -101,6 +101,7 @@
 
               for (let i = 0; i < interruptionFields.length; i++) {
                 if (
+                  plugins[i].currentPlugin.plugins[i2].interruptionData &&
                   plugins[i].currentPlugin.plugins[i2].interruptionData[
                     interruptionFields[i].id
                   ]
@@ -171,6 +172,30 @@
         />
       {/if}
     </div>
+    {#if !plugin.breadcrumb[plugin.breadcrumb.length - 1]}
+      <div class="flex justify-end">
+        <Button
+          on:click={() => deleteMainPlugin(mainPluginIndex)}
+          color="whitePrimaryIcon"
+          size="icon-square-small"
+        >
+          <span class="sr-only">{$t("close")}</span>
+          <svg
+            class="h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </Button>
+      </div>
+    {/if}
 
     {#if plugin.currentPlugin.parentPlugin && plugin.currentPlugin.parentPlugin.pluginConfig.interruptionFields}
       <svelte:component

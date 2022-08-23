20d3e2c1f9159b7fd2403e8f7d9689eb60845db1
Niels van der Molen <nielsvandermolen@gmail.com>
2022-08-23 10:35:30 +0200

    work (#62)
    Co-authored-by: Niels van der Molen <niels@wisvids.nl>

--- src/routes/curriculum.svelte
+++ src/routes/curriculum.svelte
@@ -13,12 +13,19 @@
 
 <ContainerBreadcrumpPageTitle bind:breadcrumbs title={$t("search-goals")} />
 <ListGoals>
+  <svelte-template slot="link">
   <a
-    slot="link"
+    class="ml-4 underline float-right mt-4 mb-4"
+    href="/topic/create"
+  >
+    {$t("create-new-topic")}
+  </a>
+  <a
     data-test="create-goal-link"
     class="ml-4 underline float-right mt-4 mb-4"
     href="/leerdoel/maken"
   >
     {$t("create-new-goal")}
   </a>
+</svelte-template>
 </ListGoals>
